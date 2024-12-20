83b83530c9ea1f6c6042bb1f8e4028918c41395d
Alex Chenot <alexchenot5@gmail.com>
2024-12-19 20:07:11 -0700

    refactor: Compressed token printing functionality

--- src/printing.rs
+++ src/printing.rs
@@ -3,27 +3,22 @@ use crate::types::token;
 pub fn print_token_stream(stream: &Vec<token::Token>) -> () {
     print!("[");
     for t in stream {
-        // Print static tokens
+        // Print token
         print!("{}", match t.0 {
-            token::Variant::Lambda => "LAMBDA",
-            token::Variant::LParen => "LPAREN",
-            token::Variant::RParen => "RPAREN",
-            token::Variant::Dot => "DOT",
-            token::Variant::EOF => "EOF",
-            _ => ""
-        });
-        // Print dynamic tokens
-        print!("{}", match t.0 {
+            token::Variant::Lambda => "LAMBDA".to_string(),
+            token::Variant::LParen => "LPAREN".to_string(),
+            token::Variant::RParen => "RPAREN".to_string(),
+            token::Variant::Dot => "DOT".to_string(),
+            token::Variant::EOF => "EOF".to_string(),
             token::Variant::Ident => {
                 let s = match &t.1 {
                     token::TokenValue::Str(s) => s,
                     _ => ""
                 };
                 "IDENT(".to_string() + s + ")"
-            },
-            _ => "".to_string()
+            }
         });
-        // Print semicolon
+        // Print semicolon and space
         print!("; ")
     }
     println!("]")
