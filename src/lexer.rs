be3420400d7e12cbd7c239b08754c456bf85d12f
Alex Chenot <alexchenot5@gmail.com>
2024-12-19 20:04:23 -0700

    feat: Argument parsing, --lex functionality

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,153 +1,151 @@
-pub mod lexer {
-    use regex::Regex;
-    use crate::types::token::*;
+use regex::Regex;
+use crate::types::token::*;
 
-    // Macro that puts carrot at the beginning of regex
-    macro_rules! reg {
-        ($e: expr) => {
-            concat!("^", $e)
-        }
+// Macro that puts carrot at the beginning of regex
+macro_rules! reg {
+    ($e: expr) => {
+        concat!("^", $e)
     }
+}
 
-    // Value constructor function type
-    type ValueConstructor = fn(&str) -> TokenValue;
+// Value constructor function type
+type ValueConstructor = fn(&str) -> TokenValue;
 
-    // Variant option enum
-    enum VariantOption {
-        Some(Variant, ValueConstructor),
-        None,
-        Newline
+// Variant option enum
+enum VariantOption {
+    Some(Variant, ValueConstructor),
+    None,
+    Newline
+}
+
+// Value constructor functions
+fn value_none(_: &str) -> TokenValue { TokenValue::None }
+fn value_ident(x: &str) -> TokenValue { TokenValue::Str(x.to_string()) }
+
+// Number to available tokens
+const TOKEN_COUNT: usize = 7;
+
+// Tokens
+const TOKENS: [(&str, VariantOption); TOKEN_COUNT] = [
+    (reg!(r"\\"), VariantOption::Some(Variant::Lambda, value_none)),
+    (reg!(r"\."), VariantOption::Some(Variant::Dot, value_none)),
+    (reg!(r"\("), VariantOption::Some(Variant::LParen, value_none)),
+    (reg!(r"\)"), VariantOption::Some(Variant::RParen, value_none)),
+    (reg!(r"[a-z][A-Z]+"), VariantOption::Some(Variant::Ident, value_ident)),
+    (reg!(r"\n"), VariantOption::Newline),
+    (reg!(r"(\s)+"), VariantOption::None),
+];
+
+// Lexer object
+pub struct Lexer {
+    pos: usize,
+    row: usize,
+    col: usize,
+    tokens: Vec<(Regex, VariantOption)>,
+}
+impl Lexer {
+    // Initialize new lexer
+    pub fn new() -> Lexer {
+        let mut lex = Lexer{
+            pos: 0,
+            row: 0,
+            col: 0,
+            tokens: Vec::new()
+        };
+        for t in TOKENS {
+            lex.tokens.push((
+                Regex::new(t.0).unwrap(),
+                t.1,
+            ))
+        };
+        lex
     }
-
-    // Value constructor functions
-    fn value_none(_: &str) -> TokenValue { TokenValue::None }
-    fn value_ident(x: &str) -> TokenValue { TokenValue::Str(x.to_string()) }
-
-    // Number to available tokens
-    const TOKEN_COUNT: usize = 7;
-
-    // Tokens
-    const TOKENS: [(&str, VariantOption); TOKEN_COUNT] = [
-        (reg!(r"\\"), VariantOption::Some(Variant::Lambda, value_none)),
-        (reg!(r"\."), VariantOption::Some(Variant::Dot, value_none)),
-        (reg!(r"\("), VariantOption::Some(Variant::LParen, value_none)),
-        (reg!(r"\)"), VariantOption::Some(Variant::RParen, value_none)),
-        (reg!(r"[a-z][A-Z]+"), VariantOption::Some(Variant::Ident, value_ident)),
-        (reg!(r"\n"), VariantOption::Newline),
-        (reg!(r"(\s)+"), VariantOption::None),
-    ];
-
-    // Lexer object
-    pub struct Lexer {
-        pos: usize,
-        row: usize,
-        col: usize,
-        tokens: Vec<(Regex, VariantOption)>,
-    }
-    impl Lexer {
-        // Initialize new lexer
-        pub fn new() -> Lexer {
-            let mut lex = Lexer{
-                pos: 0,
-                row: 0,
-                col: 0,
-                tokens: Vec::new()
-            };
-            for t in TOKENS {
-                lex.tokens.push((
-                    Regex::new(t.0).unwrap(),
-                    t.1,
-                ))
-            };
-            lex
-        }
-        // Next token in stream
-        fn next(&mut self, stream: &str) -> Option<Token> {
-            // Length of longest match
-            let mut longest_match: usize = 0;
-            // Index pointing to variant of longest match (initialize to zero, doesn't really matter)
-            let mut longest_variant: usize = 0;
-            // Iterate through each token, find longest match
-            for (i, token_def) in self.tokens.iter().enumerate() {
-                match token_def.0.find_at(stream, self.pos) {
-                    Some(m) => {
-                        if m.len() > longest_match {
-                            longest_match = m.len();
-                            longest_variant = i;
-                        }
-                    },
-                    None => ()
+    // Next token in stream
+    fn next(&mut self, stream: &str) -> Option<Token> {
+        // Length of longest match
+        let mut longest_match: usize = 0;
+        // Index pointing to variant of longest match (initialize to zero, doesn't really matter)
+        let mut longest_variant: usize = 0;
+        // Iterate through each token, find longest match
+        for (i, token_def) in self.tokens.iter().enumerate() {
+            match token_def.0.find_at(stream, self.pos) {
+                Some(m) => {
+                    if m.len() > longest_match {
+                        longest_match = m.len();
+                        longest_variant = i;
+                    }
+                },
+                None => ()
+            }
+        };
+        // If found token
+        if longest_match > 0 {
+            // Update col
+            self.col += longest_match;
+            // Update position
+            self.pos += longest_match;
+            // Check matched token
+            match &self.tokens.get(longest_variant).unwrap().1 {
+                // If matched usable token, get value and return
+                VariantOption::Some(var, producer) => {
+                    Some((
+                        var.clone(), // Token variant
+                        producer(&stream[self.pos..self.pos+longest_match]), // Token value
+                        (self.row, self.col - longest_match) // Token position (need to revert to old col)
+                    ))
+                },
+                // If matched throwaway token, return none
+                VariantOption::None => {
+                    None
+                },
+                // If matched newline, update row and column then return none
+                VariantOption::Newline => {
+                    self.row += 1;
+                    self.col = 0;
+                    None
                 }
-            };
-            // If found token
-            if longest_match > 0 {
-                // Update col
-                self.col += longest_match;
-                // Update position
-                self.pos += longest_match;
-                // Check matched token
-                match &self.tokens.get(longest_variant).unwrap().1 {
-                    // If matched usable token, get value and return
-                    VariantOption::Some(var, producer) => {
-                        Some((
-                            var.clone(), // Token variant
-                            producer(&stream[self.pos..self.pos+longest_match]), // Token value
-                            (self.row, self.col - longest_match) // Token position (need to revert to old col)
-                        ))
-                    },
-                    // If matched throwaway token, return none
-                    VariantOption::None => {
-                        None
-                    },
-                    // If matched newline, update row and column then return none
-                    VariantOption::Newline => {
-                        self.row += 1;
-                        self.col = 0;
-                        None
+            }
+        }
+        // Did not find token, return none
+        else {
+            None
+        }
+    }
+    // Generate stream of tokens
+    pub fn generate(&mut self, stream: &str) -> Result<Vec<Token>, String> {
+        // Reset pos, row, and column
+        self.pos = 0;
+        self.row = 0;
+        self.col = 0;
+        // Tokens vector
+        let mut tokens: Vec<Token> = Vec::new();
+        // Iterate through stream
+        loop {
+            // Save old position
+            let old_pos = self.pos;
+            // Generate next token
+            let next_token = self.next(stream);
+            // Match next token
+            match next_token {
+                Some(t) => {
+                    // Push generated token
+                    tokens.push(t)
+                },
+                None => {
+                    // If current position = old position, error
+                    if self.pos == old_pos {
+                        return Err("Unexpected token at row ".to_string() + self.row.to_string().as_ref() + ", column " + self.col.to_string().as_ref())
                     }
                 }
             }
-            // Did not find token, return none
-            else {
-                None
+            // If reached end of stream, break
+            if self.pos >= stream.len() {
+                break
             }
         }
-        // Generate stream of tokens
-        pub fn generate(&mut self, stream: &str) -> Result<Vec<Token>, String> {
-            // Reset pos, row, and column
-            self.pos = 0;
-            self.row = 0;
-            self.col = 0;
-            // Tokens vector
-            let mut tokens: Vec<Token> = Vec::new();
-            // Iterate through stream
-            loop {
-                // Save old position
-                let old_pos = self.pos;
-                // Generate next token
-                let next_token = self.next(stream);
-                // Match next token
-                match next_token {
-                    Some(t) => {
-                        // Push generated token
-                        tokens.push(t)
-                    },
-                    None => {
-                        // If current position = old position, error
-                        if self.pos == old_pos {
-                            return Err("Unexpected token at row ".to_string() + self.row.to_string().as_ref() + ", column " + self.col.to_string().as_ref())
-                        }
-                    }
-                }
-                // If reached end of stream, break
-                if self.pos >= stream.len() {
-                    break
-                }
-            }
-            // Add EOF to end of stream
-            tokens.push((Variant::EOF, TokenValue::None, (self.row, self.col)));
-            // Return
-            Ok(tokens)
-        }
+        // Add EOF to end of stream
+        tokens.push((Variant::EOF, TokenValue::None, (self.row, self.col)));
+        // Return
+        Ok(tokens)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,81 @@ mod types;
 mod lexer;
 mod printing;
 
+use clap::Parser;
+use std::io;
+use std::fs;
+use std::process;
+
+macro_rules! lex {
+    ($e1: expr, $e2: expr) => {
+        match $e1.generate($e2) {
+            Ok(s) => s,
+            Err(e) => {
+                eprintln!("{}", e);
+                process::exit(1)
+            }
+        }
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about = "Lambda calculus evaluator", long_about = None)]
+struct Args {
+    #[arg(long, group="program", help="Run in lexer mode.")]
+    lex: bool,
+
+    #[arg(long, group="program", help="Run in parse mode.")]
+    parse: bool,
+
+    #[arg(long, group="program", help="Run in script mode. Default mode if no run flags are set.")]
+    script: bool,
+
+    #[arg(help="Path to program file. Use stdin if not specified.")]
+    fname: Option<String>
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Parse CLI arguments
+    let args = Args::parse();
+
+    // Get program input
+    let input: String = match args.fname {
+        Some(p) => {
+            match fs::read_to_string(p) {
+                Ok(s) => s,
+                Err(_) => {
+                    eprintln!("file error"); 
+                    process::exit(1)
+                }
+            }
+        },
+        None => {
+            match io::read_to_string(io::stdin()) {
+                Ok(s) => s,
+                Err(_) => {
+                    eprintln!("stdin error"); 
+                    process::exit(1)
+                }
+            }
+        }
+    };
+
+    // Create lexer
+    let mut lex = lexer::Lexer::new();
+
+    // Run lexer program
+    if args.lex {
+        // Generate lexer output
+        let lexer_out = lex!(lex, &input);
+        // Print token stream
+        printing::print_token_stream(&lexer_out)
+    } 
+    // Run parser program
+    else if args.parse {
+
+    } 
+    // Run evaluator program
+    else {
+
+    }
 }
